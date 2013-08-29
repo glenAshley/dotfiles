2df8093af735071cf747584bbfbcadf8e8661a3e
Glen Ashley Peterson <glen@screenrev.com>
2013-08-29 10:06:47 +0100

    Setup script for installing apps on OS X

---
+++ setup-osx.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# open applications to install in the default browser
+open http://www.iterm2.com/
+open https://www.dropbox.com/install2
+open https://agilebits.com/downloads
+open http://www.sublimetext.com/3
+open https://www.google.com/intl/en/chrome/browser/canary.html
+open http://www.skype.com/en/download-skype/skype-for-computer/
+
+
+# open app store to install purchased apps
+open -a App\ Store
